7a5e46884be454cba0ceaca819653b3c01fdb9ea
aarav-nair <aarav.nair@gmail.com>
2024-07-22 00:32:40 -0400

    Update model.py

--- model.py
+++ model.py
@@ -2,12 +2,16 @@ from lazypredict.Supervised import LazyRegressor
 from lazypredict.Supervised import LazyClassifier
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
 
-
-# dir_path = os.path.dirname(os.path.realpath(__file__))
 df = pd.read_csv("/Users/aaravnair/FakeNewsNet 2.csv")
 x, y = df.drop('real', axis=1), df['real']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=100)
-clf = LazyClassifier(verbose=0,ignore_warnings=True, custom_metric=None)
-models, predictions = clf.fit(x_train, x_test, y_train, y_test)
-print(models)
+
+
+model = LogisticRegression()
+model.fit(x_train, y_train)
+y_pred = model.predict(x_test)
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
